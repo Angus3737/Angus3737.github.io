86c8fee03e30c2b9973f50ae31adf7dc9af3653c
Angus3737 <angusliala@gmail.com>
2025-02-26 14:14:55 -0600

    background changes on key press

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,64 +1,32 @@
-let x;
-let y;
-let rectWidth = 75;
-let rectHeight= 60;
-let dx;
-let dy;
-let r;
-let g;
-let b;
-let logo;
-
-function preload(){
-  logo = loadImage("download.png")
-}
+let newColor = "white";
 
 function setup() {
-  createCanvas(350, 400);
-  x = width/2;
-  y = height/2;
-  dx = random(-5, 5);
-  dy = random(-5, 5);
-  noStroke();
-  changeBallColor();
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
-  moveBall();
-  bounceIfNeeded();
-  displayBall();
+  backgroundChange();
 }
 
-function moveBall() {
-  //move ball
-  x += dx;   //x = x + dx;
-  y += dy;
+function backgroundChange() {
+  background(newColor);
 }
-
-function bounceIfNeeded() {
-  //bounce if needed
-  if (x> width-rectWidth|| x < 0) {
-    dx *= -1;
-    changeBallColor();
-    console.log("bounce " + x);
+function keyPressed() {
+  if (keyCode === 32) {
+    let theColors = [
+      //pastel red
+      color(230, 57, 70),
+      //dark red
+      color(164, 19, 60),
+      //bright pink
+      color(255, 112, 166),
+      //pink
+      color(255, 143, 171),
+      //pastel purple
+      color(149, 113, 134),
+      //dark purple
+      color(148, 75, 187)
+    ];
+    newColor = random(theColors);
   }
-  
-  if (y > height - rectHeight || y < 0) {
-    dy *= -1;
-    changeBallColor();
-  }
-}
-
-function displayBall() {
-  // //display ball
-  // fill(r, g, b);
-  // rect(x, y, rectWidth, rectHeight );
-  image(logo, x, y, rectWidth, rectHeight);
-}
-
-function changeBallColor() {
-  r = random(0, 255);
-  g = random(0, 255);
-  b = random(0, 255);
 }
