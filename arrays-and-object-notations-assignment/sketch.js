3b27992c24ce24c9174968766fe4cd33e4171f01
Angus3737 <angusliala@gmail.com>
2025-03-11 14:16:10 -0600

    Update sketch.js

--- arrays-and-object-notations-assignment/sketch.js
+++ arrays-and-object-notations-assignment/sketch.js
@@ -21,22 +21,12 @@ function draw() {
   // movingLine();
 }
 
-// function movingLine() {
-//   // Rotate around the y-axis.
-//   rotateY(frameCount * 0.05);
-
-//   // Draw a line.
-//   line(0, -200, 0, 200, 400, -10);
- 
-//   // Translate to the second point.
-//   translate(60, 20, -10);
-// }
 
 function keyPressed() {
   if (keyCode === 84) {
     let camX = 0;
     let camY = -800;
     let camZ = 0;
-    camera(camX, camY, camZ, 0, 0, 0, 0, 0, 1)
+    camera(camX, camY, camZ, 0, 0, 0, 0, 0, 1);
   }
 }
