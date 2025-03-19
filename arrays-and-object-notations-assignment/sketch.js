cc2323ba6de3c2f964d40912915e7e2da16134ea
Angus3737 <angusliala@gmail.com>
2025-03-19 14:17:00 -0600

    tidied the code and added planet delete key

--- arrays-and-object-notations-assignment/sketch.js
+++ arrays-and-object-notations-assignment/sketch.js
@@ -1,6 +1,6 @@
 // Solar System Simulation
 // Angus Li
-//
+// Extras for Experts:
 
 
 let sunRaysArray = [];
@@ -13,56 +13,41 @@ let p2;
 let p3;
 let p4;
 let p5;
-let p6;
-// import starsTexture from '../img/starsTexture.jpg';
-
-// const cubeTextureLoader = new THREE.CubeTextureLoader();
-// scene.background = cubeTextureLoader.load([
-//   starsTexture,
-//   starsTexture,
-//   starsTexture,
-//   starsTexture,
-//   starsTexture,
-//   starsTexture,  
-// ]);
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
-  //title
-  // p.style("font-size", "20px"); 
-
-  p = createP("Solar Sytem Simlation");
-  p.position(windowWidth/2 - 150, windowHeight/2 - 250);
+  //load audio
+  // let soundFX = createAudio('disappearsound.wav');
 
   //instructions
-  p1 = createP('Press T to view from the top position');
+  p = createP('Press T to view from the top position');
+  p.position(windowWidth/2 - 150, windowHeight/2 - 200);
+
+  p1 = createP('Press R to reset the viewing position');
   p1.position(windowWidth/2 - 150, windowHeight/2 - 150);
 
-  p2 = createP('Press R to reset the viewing position');
+  p2 = createP('Press Y to add the suns rays');
   p2.position(windowWidth/2 - 150, windowHeight/2 - 100);
-
-  p3 = createP('Press Y to add the suns rays');
-  p3.position(windowWidth/2 - 150, windowHeight/2 - 50);
   
-  p4 = createP('Press ENTER to spawn planets');
-  p4.position(windowWidth/2 - 150, windowHeight/2);
+  p3 = createP('Press ENTER to spawn planets');
+  p3.position(windowWidth/2 - 150, windowHeight/2 - 50);
 
-  p5 = createP('Use the mouse and scroll wheel to look around');
+  p4 = createP('Use the mouse and scroll wheel to look around');
+  p4.position(windowWidth/2 - 150, windowHeight/2 + 50);
+
+  p5 = createP('Press E to enter');
   p5.position(windowWidth/2 - 150, windowHeight/2 + 100);
-
-  p6 = createP('Press E to enter');
-  p6.position(windowWidth/2 - 150, windowHeight/2 + 150);
 }
 
 function lockedScreen() {
-  // creates introscreen with instructions
+  // background for the instructions
 
   background (100);
 
   fill("white");
-  rect(-200, -250, 400, 550);
+  rect(-200, -250, 400, 500);
   fill(0);
 
   if (keyIsDown(69)) {
@@ -92,7 +77,6 @@ function draw() {
     p3.hide();
     p4.hide();
     p5.hide();
-    p6.hide();
   }
 
 }
@@ -121,6 +105,13 @@ function keyPressed() {
   else if (keyCode === 13) {
     createPlanets();
   }
+  //press Backspace to get rid of previous planet, with sound effect
+  else if (keyCode === 8) {
+    let soundFX = createAudio('disappearsound.wav');
+
+    planetsArray.pop();
+    soundFX.showControls();
+  }
 }
 
 
@@ -131,15 +122,28 @@ function spawnSun() {
   stroke(255, 72, 0);
   //yellow fill
   fill(255, 170, 0);
-  sphere(110, 21);
+  sphere(130, 21);
 } 
+
+function createSunRays() {
+  //sun rays notation
+  let somesunRays= {
+    x1: 25,
+    y1: -110,
+    z1: 0,
+    x2: 200,
+    y2: 10,
+    z2: 200,
+  };
+  sunRaysArray.push(somesunRays);
+}
 
 function sunRaysAnimation(sunRays) {
   
   push();
   translate(sunRays.x1, sunRays.y1, sunRays.z1);
 
-  for (let i = 0; i < 37; i++) {
+  for (let i = 0; i < 43; i++) {
     rotateY(frameCount * 0.01);
     stroke("yellow");
     line(0, 0, 0, sunRays.x2, sunRays.y2, sunRays.z2);
@@ -148,19 +152,6 @@ function sunRaysAnimation(sunRays) {
   pop();
 }
 
-
-function createSunRays() {
-  //sun rays notation
-  let somesunRays= {
-    x1: 25,
-    y1: -90,
-    z1: 0,
-    x2: 150,
-    y2: 10,
-    z2: 200,
-  };
-  sunRaysArray.push(somesunRays);
-}
 
 function create() {
   for (let sunRays of sunRaysArray) {
@@ -172,18 +163,15 @@ function create() {
 }
 
 function createPlanets() {
-  // let orbitRadius = random(300, 1000);
-  // let startAngle = random(0, 2 * Math.PI);
 
   //planet notation
   let somePlanets= {
     r: random(20, 90),
     angle: random(0, 2 * Math.PI),
-    orbitRadius: random(300, 1000),
+    orbitRadius: random(300, 3000),
     speed: 0.01,
     y: 0,
-    detailX: random(0, 10),
-    detailY: random(0, 10),
+    detailX: random(5,7),
     colorValue: color(random(0,255), random(0,255), random(0,255)),
   };
   planetsArray.push(somePlanets);
@@ -203,24 +191,7 @@ function planetsOrbit(planets) {
   stroke(51);
   fill(planets.colorValue);
   sphere(planets.r);
-  
+  // planet.detailX
+
   pop();
-}
-
-function mousePressed() {
-  for (let i = planetsArray.length - 1; i >= 0; i--) {
-    let planet = planetsArray[i];
-
-    let planetPosition = createVector(planet.orbitRadius * cos(planet.angle),
-  planet.y, planet.orbitRadius * sin(planet.angle));
-
-    let screenPosition = screenPosition(planetPosition.x, planetPosition.y, planetPosition.z);
-
-    let distance = dist(mouseX, mouseY, screenPosition.x, screenPosition.y);
-
-    //if the planet is being clicked on
-    if (distance < planet.r) {
-      planetsArray.splice(i, 1);
-    }
-  }
 }
