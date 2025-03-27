767b5f62aa05a7691b73910b147cfd51482e081f
Angus3737 <angusliala@gmail.com>
2025-03-27 09:35:30 -0600

    started creating the grid

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -1,15 +1,33 @@
-// Project Title
-// Your Name
-// Date
+// Chinese Chess
+// Angus Li
+// April
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let cellSize;
+const SQUARE_DIMENSIONS = 10;
+let grid;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(220);
+
+  displayGrid();
+}
+
+function displayGrid() {
+  // draws a grid of rectangles of alternating colour
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      fill("black");
+      rect(x * 50, y * 50, 50, 50);
+    }
+  }
 }
