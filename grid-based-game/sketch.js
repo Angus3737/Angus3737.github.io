277d0c895149b51d5ab9314022fbe8843f6cad3f
Angus3737 <angusliala@gmail.com>
2025-04-01 14:16:32 -0600

    started shifting the board to the intersections

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -18,15 +18,15 @@ let selectedY = -1;
 let selectedPieceType = 0;
 
 let originalBoard = [
-  ['p', 'p', 'p', 'p', 'k', 'p', 'p', 'p',],
-  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',],
-  [0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0,],
-  ['river', 'river', 'river', 'river', 'river', 'river', 'river', 'river',],
-  [0, 0, 0, 0, 0, 0, 0, 0,],
-  [0, 0, 0, 0, 0, 0, 0, 0,],
-  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',],
-  ['p', 'p', 'p', 'k', 'p', 'p', 'p', 'p',]
+  ['p', 'p', 'p', 'p', 'k', 'p', 'p', 'p', 'p'],
+  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ['river', 'river', 'river', 'river', 'river', 'river', 'river', 'river', 'river'],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ['rp', 'rp', 'rp', 'rp', 'rp', 'rp', 'rp', 'rp', 'rp'],
+  ['rp', 'rp', 'rp', 'k', 'rp', 'rp', 'rp', 'rp', 'rp']
 ];
 
 function preload() {
@@ -59,14 +59,17 @@ function displayGrid() {
 function displayPieces() {
   for (let x = 0; x < cols; x++) {
     for (let y = 0; y < rows; y++) {
-      if (originalBoard[y][x] === 'p') {
-        image(pawn, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      if (originalBoard[y][x] === 'p' || originalBoard[y][x] === 'rp') {
+        image(pawn, x * CELL_SIZE - 40, y * CELL_SIZE + 40, CELL_SIZE, CELL_SIZE);
       }
       else if (originalBoard[y][x] === 'k') {
-        image(king, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        image(king, x * CELL_SIZE - 40, y * CELL_SIZE + 40, CELL_SIZE, CELL_SIZE);
       }
       else if (originalBoard[y][x] === 'river') {
         image(water, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+
+      }
+      else if (originalBoard[y][x] === "color") {
 
       }
       
@@ -79,27 +82,27 @@ function mousePressed() {
   let y = Math.floor(mouseY/CELL_SIZE);
   if (x >= 0 && x <= cols && y >= 0 && y < rows) {
     if (pieceSelected) {
-      movePawn(selectedX, selectedY, x, y)
+      movePawn(selectedX, selectedY, x, y);
       pieceSelected = false;
-      selectedX = -1;
-      selectedY = -1;
-      selectedPieceType = 0;
+      // originalBoard[y-1][x-1] = "color";
     }
     else if (originalBoard[y][x] !== 0 && originalBoard[y][x] !== "river") {
       pieceSelected = true;
       selectedX = x;
       selectedY = y;
       selectedPieceType = originalBoard[y][x];
+
     }
   }
 }
 
 function movePawn(oldX, oldY, newX, newY) {
-  if (originalBoard[newY][newX] === 0) {
-    originalBoard[newY][newX] = originalBoard[oldY][oldX]
+  if (originalBoard[newY][newX] === 0 || originalBoard[newY][newX] === "river") {
+    originalBoard[newY][newX] = originalBoard[oldY][oldX];
     originalBoard[oldY][oldX] = 0;
   }
 }
+
 
 function highlightMoves(originalBoard) {
   let x = originalBoard.x;
